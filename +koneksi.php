5ccb0504b5be1bde1b94871a899a656f602617e2
meramissembiring <71537664+meramissembiring@users.noreply.github.com>
2020-12-16 23:59:47 +0800

    Update +koneksi.php

--- +koneksi.php
+++ +koneksi.php
@@ -3,12 +3,6 @@ $host = 'localhost';
 $db = 'data-kendaraan';
 $user = 'root';
 $pass = '12';
- 
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
-    
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo $e->getMessage();
+
 }
 ?>
